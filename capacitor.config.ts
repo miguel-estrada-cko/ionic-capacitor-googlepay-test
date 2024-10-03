8501c3f5af319749d8ee99298a6a5fc772815a7a
Miguel Estrada <miguel.estrada@checkout.com>
2024-10-03 10:45:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pay-plugin',
+  webDir: 'www'
+};
+
+export default config;
